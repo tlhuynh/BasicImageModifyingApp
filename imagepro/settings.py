b3af11936a12e1e719e9ead69e61b0d064df4151
tuanh <tuan.huynh@temple.edu>
2020-03-24 20:00:40 -0400

    Update to use with pycharm

--- imagepro/settings.py
+++ imagepro/settings.py
@@ -82,7 +82,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
 
 
 # Absolute path to the directory static files should be collected to.
@@ -93,7 +93,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'myapp', 'templates', 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://127.0.0.1:8000/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
